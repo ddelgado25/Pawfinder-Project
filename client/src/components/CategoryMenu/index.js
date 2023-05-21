07d4818ea180e8c89ab352be6d0aa20fc2984692
alissonpceva <axc2980@miami.edu>
2023-05-21 18:25:56 -0400

    modified home.js and category index.js to search for breed and update category menu

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,61 +1,27 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
-import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-
-function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
-
-  const { categories } = state;
-
-  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
-
-  useEffect(() => {
-    if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
-      categoryData.categories.forEach((category) => {
-        idbPromise('categories', 'put', category);
-      });
-    } else if (!loading) {
-      idbPromise('categories', 'get').then((categories) => {
-        dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories,
-        });
-      });
-    }
-  }, [categoryData, loading, dispatch]);
-
-  const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
+import React from 'react';
+​
+function CategoryMenu({ handleCategorySelect }) {
+  const handleClick = (category) => {
+    handleCategorySelect(category);
   };
-
+​
+  const categories = [
+    { name: 'Golden Retriever' },
+    { name: 'French Bulldog' },
+    { name: 'Maltipoos' },
+    { name: 'English Shepherd' },
+  ];
+​
   return (
     <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            handleClick(item._id);
-          }}
-        >
-          {item.name}
+      <h2>Choose a Breed:</h2>
+      {categories.map((category) => (
+        <button key={category.name} onClick={() => handleClick(category.name)}>
+          {category.name}
         </button>
       ))}
     </div>
   );
 }
-
+​
 export default CategoryMenu;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-
+​
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
-
+​
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
+​
+  const handleCategorySelect = (category) => {
+    setSearchTerm(category);
+  };
+​
   return (
     <div className="container">
       <input
@@ -18,23 +22,12 @@ const Home = () => {
         onChange={handleSearch}
         placeholder="Search categories"
       />
-      <CategoryMenu />
+      <CategoryMenu handleCategorySelect={handleCategorySelect} />
       <ProductList searchTerm={searchTerm} />
       <Cart />
-      <button
-        style={{
-          backgroundColor: "blue",
-          color: "white",
-          padding: "10px 20px",
-          border: "none",
-          borderRadius: "4px",
-          cursor: "pointer",
-        }}
-      >
-        Home
-      </button>
+​
     </div>
   );
 };
-
+​
 export default Home;
