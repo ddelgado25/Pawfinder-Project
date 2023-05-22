ed7530cd2d4cfbe51ceb4579585466221461a6e2
alissonpceva <axc2980@miami.edu>
2023-05-22 19:16:19 -0400

    fixed bug in productitem

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,77 +1,70 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
-function ProductList({ searchTerm }) {
+import React from "react";
+import { Link } from "react-router-dom";
+import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+import { BsCart4 } from 'react-icons/bs';
+function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const { currentCategory, products } = state;
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
-  const filterProducts = useCallback(() => {
-    let filtered = products;
-    if (currentCategory) {
-      filtered = filtered.filter(
-        (product) => product.category._id === currentCategory
-      );
-    }
-    if (searchTerm) {
-      filtered = filtered.filter(
-        (product) =>
-          product.name.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-    }
-    setFilteredProducts(filtered);
-  }, [currentCategory, products, searchTerm]);
-  useEffect(() => {
-    if (searchTerm === "") {
-      setFilteredProducts([]);
-    } else {
-      filterProducts();
-    }
-  }, [searchTerm, filterProducts]);
-  useEffect(() => {
-    if (data) {
+  const {
+    image,
+    name,
+    _id,
+    price,
+    quantity
+  } = item;
+  const { cart } = state
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    if (itemInCart) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
-    } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
-        });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 }
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
-  }, [data, loading, dispatch]);
+  }
   return (
-    <div className="my-2">
-      <h2>Our Products:</h2>
-      {searchTerm !== "" && filteredProducts.length ? (
-        <div className="flex-row">
-          {filteredProducts.map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+      <div className="bg-white rounded-xl shadow-md overflow-hidden transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl p-2">
+        <Link to={`/products/${_id}`} className="block">
+          <div className="h-48">
+            <img
+              alt={name}
+              src={`/images/${image}`}
+              className="w-full h-full object-cover rounded-xl"
             />
-          ))}
+          </div>
+          <p className="mt-2 text-gray-800 font-semibold">{name}</p>
+        </Link>
+        <div className="flex justify-between items-center mt-2">
+          <div className="text-sm text-gray-600">
+            {quantity} {quantity === 1 ? 'item' : 'items'} in stock
+          </div>
+          <span className="text-lg font-semibold text-gray-800">${price}</span>
         </div>
-      ) : searchTerm !== "" ? (
-        <h3>No products found.</h3>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+        <button
+          onClick={addToCart}
+          className="block w-full mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 transition-colors duration-300 ease-in-out flex items-center justify-center"
+        >
+          <span className="mr-2">
+            <BsCart4 size={20} />
+          </span>
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 }
-export default ProductList;
+export default ProductItem;
