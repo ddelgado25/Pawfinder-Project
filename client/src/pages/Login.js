9c2fc9a1b246148fad719a1cd7e9384705dfb5de
alissonpceva <axc2980@miami.edu>
2023-05-23 19:48:12 -0400

    added picture for log in page, added css and modify styling for log in page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,8 +3,8 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
-
-
+import helloDog from '/Users/Alisson/Bootcamp/Pawfinder-Project/client/src/images/hello.jpg';
+import '/Users/Alisson/Bootcamp/Pawfinder-Project/client/src/loginstyle.css'; // Import the updated CSS file
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -33,46 +33,56 @@ function Login(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+      <Link to="/signup" className="go-to-signup">
+        ← Go to Signup
+      </Link>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
+      <div className="login-container">
+        <div className="form-container">
+          <h2 className="login-title">Login</h2>
+          <form onSubmit={handleFormSubmit}>
+            <div className="flex-row space-between">
+              <label htmlFor="email">Email address:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                className="input-field"
+                style={{ width: '100%' }} // Set the width of the input field
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                className="input-field"
+                style={{ width: '100%' }} // Set the width of the input field
+                onChange={handleChange}
+              />
+            </div>
+            {error && (
+              <div>
+                <p className="error-text">The provided credentials are incorrect</p>
+              </div>
+            )}
+            <div className="flex-row flex-end">
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-      <h1 className = "text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      </div>
+
+      <div>
+        <img src={helloDog} alt="pitbull" />
+      </div>
     </div>
-
-
-
   );
 }
 
