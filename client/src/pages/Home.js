ee8fa64280d2cb0ff75c3dc5e03c4d5b01f8b921
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-05-24 00:44:06 -0400

    fixed sizing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
   return (
     <div className="container mx-auto">
       <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-2/3 pr-6 flex flex-col mt-10" style={{ marginLeft: "-20px" }}>
+        <div className="w-full md:w-2/4 pr-6 flex flex-col mt-10" style={{ marginLeft: "-20px" }}>
           <input
             type="text"
             value={searchTerm}
@@ -34,6 +34,7 @@ const Home = () => {
             placeholder="Search breeds..."
             className="border-4 border-custom-green bg-transparent rounded px-4 py-2 mb-4 md:mb-1 w-full"
           />
+
           <CategoryMenu handleCategorySelect={handleCategorySelect} className="mt-4  " />
           <p className="text-custom-blue text-8xl font-bold mb-2 mt-6">YOUR NEW</p>
           <p className="text-custom-green text-8xl font-bold mb-2">PAW MATE </p>
@@ -41,7 +42,7 @@ const Home = () => {
         </div>
 
         <div className="w-full md:w-1/2 flex justify-center items-center">
-          <img src="/images/pitbull-homepage.png" alt="pitbull" className="h-auto mx-auto" style={{ maxWidth: "100%" }} />
+          <img src="/images/pitbull-homepage.png" alt="pitbull" className="h-auto mx-auto" style={{ maxWidth: "85%" }} />
         </div>
       </div>
       <div ref={productListRef}>
